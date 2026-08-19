c9c7f5c1bababfd8bb57a616ed442d20fa294fae
chandra-pi <chandrahyd.45@gmail.com>
2026-08-19 19:33:00 +0530

    terraform ansible

--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z0906595WCJPC66S1LJS"
+}
+
+variable "zone_name" {
+    default = "devaws84s.online"
+}
