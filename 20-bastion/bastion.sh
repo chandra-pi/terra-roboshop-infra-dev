8611886c9a9bc76a6732f6e423a6a8a08b91a7a4
chandra-pi <chandrahyd.45@gmail.com>
2026-08-20 23:12:26 +0530

    terraform ansible

---
+++ 20-bastion/bastion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+### first installing utils, adding terraform repo here, and installing terraform
+yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
+
+growpart /dev/nvme0n1 4
+lvextend -L +20G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/homeVol
+
+xfs_growfs /
+xfs_growfs /home
--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,7 @@
 module "component" {
     for_each = var.components
-    source = "../../terraform-aws-roboshop" ### this is referring from local directory
+    ## source = "../../terraform-aws-roboshop" ### this is referring from local directory
+    source = "git::https://github.com/chandra-pi/terraform-aws-roboshop.git?ref=main"  #### referring from git
     component = each.key
     rule_priority = each.value.rule_priority
 }
